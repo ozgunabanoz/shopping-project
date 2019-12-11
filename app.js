55d2c9d5d7bc6487bd9db0a6f9db68370095ab4d
Ozgun <ozgun.abanoz@gmail.com>
2019-12-11 14:04:01 +0300

    Creating and managing a dummy user

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,16 +20,44 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(async (req, res, next) => {
+  let user;
+
+  try {
+    user = await User.findByPk(1);
+
+    req.user = user;
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize
-  .sync()
-  .then(res => {
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+(async () => {
+  let user;
+
+  try {
+    await sequelize.sync({ force: true });
+
+    user = await User.findByPk(1);
+
+    if (!user) {
+      user = await User.create({
+        name: 'Ozgun',
+        email: 'a@a.com'
+      });
+    }
+
     app.listen(process.env.PORT || 3000);
-  })
-  .catch(err => {
+  } catch (err) {
     console.log(err);
-  });
+  }
+})();
