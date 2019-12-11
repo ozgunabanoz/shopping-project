ba6df670eb5977d24a934cc09e5831453a95111b
Ozgun <ozgun.abanoz@gmail.com>
2019-12-11 10:23:00 +0300

    Adding a product.

--- controllers/admin.js
+++ controllers/admin.js
@@ -9,25 +9,20 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = async (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
-  const description = req.body.description;
-  const product = new Product(
-    null,
-    title,
-    imageUrl,
-    description,
-    price
-  );
+  const { title, imageUrl, price, description } = req.body;
 
   try {
-    await product.save();
+    await Product.create({
+      title,
+      price,
+      imageUrl,
+      description
+    });
+
+    console.log('Product created.');
   } catch (err) {
     console.log(err);
   }
-
-  res.redirect('/');
 };
 
 exports.getEditProduct = (req, res, next) => {
