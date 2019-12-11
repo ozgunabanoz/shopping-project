400dbde61ac85a7c61c280b4b772551890c9f263
Ozgun <ozgun.abanoz@gmail.com>
2019-12-11 09:58:02 +0300

    Connecting to db with sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PW
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  { dialect: 'mysql', host: process.env.DB_HOST }
+);
 
-module.exports = pool.promise();
+module.exports = sequelize;
