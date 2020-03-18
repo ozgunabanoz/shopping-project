2de0b43d84c82a7264769957af604fbb7261d78f
Ozgun <ozgun.abanoz@gmail.com>
2020-03-18 18:03:26 +0300

    Package update.

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ app.use((error, req, res, next) => {
 (async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    app.listen(process.env.PORT || 3000);
+    app.listen(process.env.PORT || 5000);
   } catch (err) {
     console.log(err);
   }
